0f5b2f613a4a25e7caee85d342c2d66ad11e5179
holyfs <egcg1986@gmail.com>
2022-07-08 10:43:49 +0000

    comentario de prueba routes, line 19

--- src/api/routes.py
+++ src/api/routes.py
@@ -14,9 +14,9 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
-
+    
+#comentario prueba
 #USER METHODS
 @api.route('/signup', methods=['POST'])
 def add_user():
