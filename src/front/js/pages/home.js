ece4b238fe111970cf3f3d1adffc2d6d2dda3570
ayo107 <ayo_linkin10@hotmail.com>
2022-07-18 17:12:03 +0000

    sin cambios relevantes

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,3 +1,4 @@
+
 import React, { useContext, useState, useEffect, Component } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
@@ -11,41 +12,41 @@ import Pagination from "../component/Pagination";
 import Filter from "../component/Filter/filter";
 
 export const Home = () => {
-	let [pageNumber, updatePageNumber] = useState(1);
-	let [gender, updateGender] = useState("");
-	let [search, setSearch] = useState("");
-	let [fetchedData, updateFetchedData] = useState([]);
-	let { info, results } = fetchedData;
-	const { store, actions } = useContext(Context);
-	let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&gender=${gender}`;
-	
-	
-	useEffect(() => {
-		(async function () {
-		  let data = await fetch(api).then((res) => res.json());
-		  updateFetchedData(data);
-		})();
-	  }, [api]);
-	
-	  return (
-		
-	<div className="App">
+    let [pageNumber, updatePageNumber] = useState(1);
+    let [gender, updateGender] = useState("");
+    let [search, setSearch] = useState("");
+    let [fetchedData, updateFetchedData] = useState([]);
+    let { info, results } = fetchedData;
+    const { store, actions } = useContext(Context);
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&gender=${gender}`;
+
+
+    useEffect(() => {
+        (async function () {
+          let data = await fetch(api).then((res) => res.json());
+          updateFetchedData(data);
+        })();
+      }, [api]);
+
+      return (
+
+    <div className="App">
   <h1 className="text-center mb-3">Músicos/Banda</h1>
   <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
   <div className="container">
   <div className="row">
-   
+
   <Filter
   pageNumber={pageNumber}
   updateGender={updateGender}
   updatePageNumber={updatePageNumber}
-	/>
-	
+    />
+
     <div className="col-lg-8 col-12">
       <div className="row">
-	<Card results={results} />
-		
-       
+    <Card results={results} />
+
+
       </div>
     </div>
   </div>
