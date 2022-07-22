b71b70ebb010c69c2bf9fbb15bb6b9478987a835
gelomamo <angelfdezfdez87@gmail.com>
2022-07-22 16:44:46 +0000

    random no graba

--- src/api/commands.py
+++ src/api/commands.py
@@ -82,29 +82,36 @@ def setup_commands(app):
 
     @app.cli.command("insert-user-instruments") # name of our command
     def insert_test_data():
-        instrument_id = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38]
         user_id = [1,2,3,4,5,6]
         for x in user_id:
-            # user_instrumentos = Instruments_user()
-            random_instrument= random.sample(instrument_id, random.randint(1,8))
-            random_user = x
-            print(random_instrument)
-            print(random_user)
-            # db.session.add(user_instrumentos)
-            # db.session.commit()
+            num_instruments = random.randint(1,6)
+            for y in range(1,num_instruments):
+                user_instrumentos = Instruments_user()
+                random_instrument= random.randint(1, 38)
+                random_user = x
+                user_instrumentos.instruments_id = random_instrument
+                user_instrumentos.user_id = random_user
+                print(random_instrument)
+                print(random_user)
+                db.session.add(user_instrumentos)
+                db.session.commit()
 
     @app.cli.command("insert-user-genres") # name of our command
     def insert_test_data():
-        genre_id = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        
         user_id = [1,2,3,4,5,6]
         for x in user_id:
-            # user_generos = Generos_user()
-            random_genre= random.sample(genre_id, random.randint(1,8))
-            random_user = x
-            print(random_genre)
-            print(random_user)
-            # db.session.add(user_generos)
-            # db.session.commit()
+            num_genre = random.randint(1,6)
+            for y in range(1,num_genre):
+                user_generos = Generos_user()
+                random_genre= random.randint(1,16)
+                random_user = x
+                user_generos.genre_id = random_genre
+                user_generos.user_id = random_user
+                print(random_genre)
+                print(random_user)
+                db.session.add(user_generos)
+                db.session.commit()
     
 
 
