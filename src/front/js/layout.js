2a3eab3c88d241fc92b7a48e811a2a67dde48003
gelomamo <angelfdezfdez87@gmail.com>
2022-07-30 10:37:26 +0000

    cambio de password no modal

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -56,8 +56,8 @@ const Layout = () => {
                         <Route element={<Redirect />} path="/rederictsignup" />
                         <Route element={<ForgetPassword/>} path="/forgetpassword" />
                         <Route element={<Images/>} path="/images" />
-                        <Route element={<ContactNoModal/>} path="/ContactNoModal:id" />
-                        <Route element={<ChangePasswordNoModal/>} path="/ChangePasswordNoModal:id" />
+                        <Route element={<ContactNoModal/>} path="/ContactNoModal" />
+                        <Route element={<ChangePasswordNoModal/>} path="/ChangePasswordNoModal" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
