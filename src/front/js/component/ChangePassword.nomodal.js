63ff080b7001ec7fcd5e8f7132fce4e1bdf191fc
gelomamo <angelfdezfdez87@gmail.com>
2022-08-01 16:16:47 +0000

    cambios en bio

--- src/front/js/component/ChangePassword.nomodal.js
+++ src/front/js/component/ChangePassword.nomodal.js
@@ -4,6 +4,8 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, L
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from "react-router-dom";
 import config from '../config';
+import Swal from "sweetalert2";
+
 
 export const ChangePasswordNoModal = () => {
 
@@ -17,46 +19,56 @@ export const ChangePasswordNoModal = () => {
       alert("Las contraseñas no coinciden");
       return;
     }
-    fetch(config.hostname + `/user/${id}/new-password`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "multipart/form-data",
-      },
-      body: ({
-        password: password,
-        new_password: newpassword
+    else {
+      const response = await fetch(config.hostname + `/user/${id}/new-password`, {
+        method: "PUT",
+        headers: {
+          "mode": 'no-cors'
+        },
+        body: ({
+          password: password,
+          new_password: newpassword
+        })
+      });
+      const responseJson = await response.json();
+      Swal.fire({
+        icon: 'sucess',
+        title: 'Contraseña guardada',
+
+      }).then(() => {
+        window.location.href = '/personalbio:' + id
       })
-    });
-    // necesitaria un RETURN aquí para cerrar y enviar un alert con "cambio de contraseña"
+      return responseJson;
+    }
   }
 
-  return (
-    <>
-      <div class="form-group">
-        <input type="password" class="form-control" onChange={event => setPassword(event.target.value)} required />
-        <label class="form-control-placeholder" for="password">Antigua Password</label>
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" onChange={event => setNewPassword(event.target.value)} required />
-        <label class="form-control-placeholder" for="password">Nueva Password</label>
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" onChange={event => setConfirmPass(event.target.value)} required />
-        <label class="form-control-placeholder" for="password">Nueva Password</label>
-      </div>
+    return (
+      <>
+        <div class="form-group">
+          <input type="password" class="form-control" onChange={event => setPassword(event.target.value)} required />
+          <label class="form-control-placeholder" for="password">Antigua Password</label>
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" onChange={event => setNewPassword(event.target.value)} required />
+          <label class="form-control-placeholder" for="password">Nueva Password</label>
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" onChange={event => setConfirmPass(event.target.value)} required />
+          <label class="form-control-placeholder" for="password">Nueva Password</label>
+        </div>
 
-      <div className="d-flex justify-content-center mt-3 mb-2" >
-        <button onClick={() => ChangePassword()}>Cambiar contraseña</button>
-      </div>
-      <div className="d-flex justify-content-end">
-        <form>
-          <a href={"/personalbio:"+id}>
-            <input type="button" value="Cerrar" />
-          </a>
-        </form>
-      </div>
-    </>
-  )
+        <div className="d-flex justify-content-center mt-3 mb-2" >
+          <button onClick={() => ChangePassword()}>Cambiar contraseña</button>
+        </div>
+        <div className="d-flex justify-content-end">
+          <form>
+            <a href={"/personalbio:" + id}>
+              <input type="button" value="Cerrar" />
+            </a>
+          </form>
+        </div>
+      </>
+    )
 
-}
+  }
 
